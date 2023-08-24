a391a5ad79ca4048f363428d2960e1781ee492c7
Ryan Hamilton <github@ryan-h.com>
2023-08-24 20:56:20 +0100

    Add apache 2 license.

--- qstudio/src/main/java/com/timestored/qstudio/QStudioLauncher.java
+++ qstudio/src/main/java/com/timestored/qstudio/QStudioLauncher.java
@@ -1,3 +1,19 @@
+/*
+ * qStudio - Free SQL Analysis Tool
+ * Copyright C 2013-2023 TimeStored
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.timestored.qstudio;
 
 import java.awt.Color;
@@ -161,7 +177,7 @@ public class QStudioLauncher  {
 
 				AppLaunchHelper.setMacAndWindowsAppearance(title);
 				AppLaunchHelper.setTheme(MyPreferences.INSTANCE.getCodeTheme());
-				AppLaunchHelper.logToUsersFolder("qStudio");
+				AppLaunchHelper.logToUsersFolder(QStudioFrame.APP_TITLE);
 				LOG.info("Starting QStudioLauncher  launch() ###################################");
 				LOG.info("System.getProperty(\"java.version\")=" + System.getProperty("java.version"));
 
