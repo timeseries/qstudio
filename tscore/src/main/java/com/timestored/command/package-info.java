5db25e1962ed4c126ae842061b0e2b5cf7021a63
Ryan Hamilton <github@ryan-h.com>
2024-11-18 20:17:57 +0000

    Delete tscore.

--- tscore/src/main/java/com/timestored/command/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * A CommandBar that allows fuzzy searching all possible commands in an application.
- */
-package com.timestored.command;
--- tscore/src/main/java/com/timestored/jgrowl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides Growl alerts, pop-up alerts that by default appear on
- * one side of the parent window and fade out after a period of time.
- * Intended as a less intrusive alternative to message boxes. 
- * Also similar in style to some existing web frameworks.
- */
-package com.timestored.jgrowl;
